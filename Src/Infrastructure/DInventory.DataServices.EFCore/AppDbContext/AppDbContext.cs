29ca36eed780edde0871bb71b3bd025f306f3665
K Hossain <tahs.sumon@gmail.com>
2022-05-12 18:43:44 +0100

    added BbContext Table

---
+++ Src/Infrastructure/DInventory.DataServices.EFCore/AppDbContext/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DInventory.Domains.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DInventory.DataServices.EFCore.AppDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet <Product> Products { get; set; }
+
+    };
+}
+
+
